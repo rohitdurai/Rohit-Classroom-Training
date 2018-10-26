fece9985509c8eb561938a981f74b03fc271e360
rohitdurai <43957242+rohitdurai@users.noreply.github.com>
2018-10-26 16:14:46 +0530

    Add files via upload

---
+++ src/Assessment_Demo_Solution/src/com/BOOK/BookDAO.java
@@ -0,0 +1,11 @@
+package com.BOOK;
+
+import java.util.List;
+
+
+    public interface BookDAO {
+        void add(Book book);
+        Book findById(int bookId);
+        List<Book> findByAuthor(String author);
+    }
+
