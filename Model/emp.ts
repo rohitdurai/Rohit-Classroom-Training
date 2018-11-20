dbe37bd1b365a3d6c51b0b18f9396b9693a10d25
rohitdurai <43957242+rohitdurai@users.noreply.github.com>
2018-11-20 17:07:21 +0530

    Add files via upload

---
+++ Model/emp.ts
@@ -0,0 +1,12 @@
+export class emp{
+    id: number;
+    name:string;
+    gender: string;
+    email?: string;
+    phonenumber?: number;
+    contactPreference: string;
+    dateOfBirth: Date;
+    department: string;
+    isActive: boolean;
+    photoPath: string;
+}
